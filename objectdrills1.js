5ed89ef09788dd1fbcbb4d1adc63bd28b1604acf
JT Liner <[jtliner925@gmail.com]>
2019-12-03 15:16:24 -0600

    personMaker function

--- objectdrills1.js
+++ objectdrills1.js
@@ -1,27 +1,40 @@
 'use strict';
 
 function createMyObject () {
-    return { 
+  return { 
     foo: 'bar',
     answerToUniverse: 42,
     'olly olly': 'oxen free',
     sayHello: function () {
-        return "hello";
+      return "hello";
     },}
     
 }
 
-console.log(createMyObject());
+//console.log(createMyObject());
 
 let obj = {};
 
 function updateObject(obj) {
-    //let obj = {};
-    obj.foo = "foo";
-    obj.bar = "bar";
-    obj.bizz = "bizz";
-    obj.bang = "bang";
-    return obj;
+  //let obj = {};
+  obj.foo = "foo";
+  obj.bar = "bar";
+  obj.bizz = "bizz";
+  obj.bang = "bang";
+  return obj;
 }
 
-console.log(updateObject(obj));
+//console.log(updateObject(obj));
+
+function personMaker() {
+  var person = {
+    firstName: 'Paul',
+    lastName: 'Jones',
+    // replace `null` with a function that uses self reference to return
+    // full name
+    fullName: function(){return `${this.firstName} ${this.lastName}`;},
+    
+  };
+  return person;
+}
+console.log(personMaker());
